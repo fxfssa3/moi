f7e173530117270ce3be32c374d6af1e61bb253b
SnaksiXD <aleksin.roskat@gmail.com>
2023-11-28 18:10:54 +0200

    kameran kääntö lisätty

--- siu/Assets/pelaaja.cs
+++ siu/Assets/pelaaja.cs
@@ -6,20 +6,32 @@ public class pelaaja : MonoBehaviour
 {
     public float moveSpeed = 5.5f;
     public CharacterController controller;
+    public float mouseSpeed = 3f;
+    public CursorLockMode cursorLock;
+    private float mouseMovementX  = 0f;
+    private float mouseMovementY  = 0f;
     Vector3 moveDir;
     // Start is called before the first frame update
     void Start()
     {
-        
+      Cursor.lockState = cursorLock;
+      Cursor.visible =(CursorLockMode.Locked != cursorLock);
     }
 
     // Update is called once per frame
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
-        float vertical = input.GetAxis("vertical")
+        float vertical = Input.GetAxis("Vertical");
+        mouseMovementY += Input.GetAxis("Mouse Y") * moveSpeed;
+        mouseMovementX -= Input.GetAxis("Mouse X") * moveSpeed;
 
-        moveDir = new Vector3(horizontal,0,0);
+        
+        Camera.main.transform.localRotation = Quaternion.Euler(mouseMovementY,0,0);
+        transform.localRotation = Quaternion.Euler(0,mouseMovementX,0);
+
+        moveDir = new Vector3(horizontal,0,vertical);
+        moveDir = transform.rotation * moveDir;
 
         controller.SimpleMove(moveDir * moveSpeed);
     }
